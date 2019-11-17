94c1dc409b3299b5423c52a501db1f25e2d97b8d
Yaroslav Yadrishnikov <y.yadrushnikov@corp.vk.com>
2019-11-17 20:52:22 +0300

    edit config fix

--- qbittorrent/rootfs/etc/cont-init.d/qbittorrent.sh
+++ qbittorrent/rootfs/etc/cont-init.d/qbittorrent.sh
@@ -3,19 +3,19 @@
 # Hass.io Add-ons: qBittorrent
 # Configures qBittorrent before running
 # ==============================================================================
-readonly conf=/config/qBittorrent/qBittorrent.conf
+# readonly conf=/config/qBittorrent/qBittorrent.conf
 
-if ! bashio::config.has_value "save_path"; then
-    bashio::log.fatal
-    bashio::log.fatal "Add-on configuration is incomplete!"
-    bashio::log.fatal
-    bashio::log.fatal "qBittorrent requires a claim code on the first run!"
-    bashio::log.fatal
-    bashio::log.fatal "Please check the installation manual of the add-on."
-    bashio::log.fatal
-    bashio::exit.nok
-fi
+# if ! bashio::config.has_value "save_path"; then
+#     bashio::log.fatal
+#     bashio::log.fatal "Add-on configuration is incomplete!"
+#     bashio::log.fatal
+#     bashio::log.fatal "qBittorrent requires a claim code on the first run!"
+#     bashio::log.fatal
+#     bashio::log.fatal "Please check the installation manual of the add-on."
+#     bashio::log.fatal
+#     bashio::exit.nok
+# fi
 
-save_path=$(bashio::config 'save_path')
-bashio::log.info "Editing Save Path"
-sed -i -e '/Downloads\\SavePath=/ s/=.*/=${save_path}/' $conf
+# save_path=$(bashio::config 'save_path')
+# bashio::log.info "Editing Save Path"
+# sed -i -e '/Downloads\\SavePath=/ s/=.*/=${save_path}/' $conf
