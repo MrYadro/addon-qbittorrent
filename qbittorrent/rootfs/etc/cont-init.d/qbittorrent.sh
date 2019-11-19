65f8664b17c731513b2dadce93cf8fd8ec1911aa
Yaroslav Yadrishnikov <y.yadrushnikov@corp.vk.com>
2019-11-19 14:48:58 +0300

    edit config fix

--- qbittorrent/rootfs/etc/cont-init.d/qbittorrent.sh
+++ qbittorrent/rootfs/etc/cont-init.d/qbittorrent.sh
@@ -6,16 +6,17 @@
 readonly conf=/config/qBittorrent/config/qBittorrent.conf
 readonly defconf=/defaults/qBittorrent.conf
 
-# if ! bashio::config.has_value "save_path"; then
-#     bashio::log.fatal
-#     bashio::log.fatal "Add-on configuration is incomplete!"
-#     bashio::log.fatal
-#     bashio::log.fatal "qBittorrent requires a claim code on the first run!"
-#     bashio::log.fatal
-#     bashio::log.fatal "Please check the installation manual of the add-on."
-#     bashio::log.fatal
-#     bashio::exit.nok
-# fi
+# Check legal notice to be accepted
+if ! bashio::config.true "legal_notice_accepted"; then
+    bashio::log.fatal
+    bashio::log.fatal "Add-on configuration is incomplete!"
+    bashio::log.fatal
+    bashio::log.fatal "qBittorrent requires you to accept legal notice!"
+    bashio::log.fatal
+    bashio::log.fatal "Please check the installation manual of the add-on."
+    bashio::log.fatal
+    bashio::exit.nok
+fi
 
 # Ensure configuration folder exists
 if ! bashio::fs.directory_exists "$(dirname "${conf}")"; then
@@ -23,8 +24,3 @@ if ! bashio::fs.directory_exists "$(dirname "${conf}")"; then
         || bashio::exit.nok "Failed to create qBittorrent configuration directory"
     cp $defconf $conf
 fi
-
-save_path=$(bashio::config 'save_path')
-save_path_param="Downloads\SavePath"
-bashio::log.info "Editing Save Path"
-sed -i "s/^\($save_path_param\s*=\s*\).*\$/\1$save_path/"
